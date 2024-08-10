49d67391ce149ef4ac6ce9cea275f32802dac0a9
DontStarve72 <helinos67@protonmail.com>
2024-08-10 00:24:59 -0700

    Port it yourself  ¯\_(ツ)_/¯

--- src/main/java/qinomed/kubejsdelight/KubeJSDelightPlugin.java
+++ src/main/java/qinomed/kubejsdelight/KubeJSDelightPlugin.java
@@ -2,25 +2,14 @@ package qinomed.kubejsdelight;
 
 import dev.latvian.mods.kubejs.KubeJSPlugin;
 import dev.latvian.mods.kubejs.recipe.schema.RegisterRecipeSchemasEvent;
-import dev.latvian.mods.kubejs.registry.RegistryInfo;
-import net.minecraft.resources.ResourceLocation;
-import qinomed.kubejsdelight.block.custom.FeastBlockBuilder;
-import qinomed.kubejsdelight.item.custom.KnifeItemBuilder;
-import qinomed.kubejsdelight.block.custom.PieBlockBuilder;
+import net.minecraft.util.Identifier;
 import qinomed.kubejsdelight.recipe.CookingRecipeJS;
 import qinomed.kubejsdelight.recipe.CuttingRecipeJS;
 
 public class KubeJSDelightPlugin extends KubeJSPlugin {
     @Override
-    public void init() {
-        RegistryInfo.ITEM.addType("farmersdelight:knife", KnifeItemBuilder.class, KnifeItemBuilder::new);
-        RegistryInfo.BLOCK.addType("farmersdelight:pie", PieBlockBuilder.class, PieBlockBuilder::new);
-        RegistryInfo.BLOCK.addType("farmersdelight:feast", FeastBlockBuilder.class, FeastBlockBuilder::new);
-    }
-
-    @Override
     public void registerRecipeSchemas(RegisterRecipeSchemasEvent event) {
-        event.register(new ResourceLocation("farmersdelight:cutting"), CuttingRecipeJS.SCHEMA);
-        event.register(new ResourceLocation("farmersdelight:cooking"), CookingRecipeJS.SCHEMA);
+        event.register(new Identifier("farmersdelight:cutting"), CuttingRecipeJS.SCHEMA);
+        event.register(new Identifier("farmersdelight:cooking"), CookingRecipeJS.SCHEMA);
     }
 }
